bb18686be4b4f25bea2ddafaa786ea542e6eddfb
Bojtronic <bojtronic@gmail.com>
2024-12-06 15:26:40 -0600

    Modulo de canciones

--- LaVentaMusical/LaVentaMusical/Controllers/CancionesController.cs
+++ LaVentaMusical/LaVentaMusical/Controllers/CancionesController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LaVentaMusical.Models;
 
 namespace LaVentaMusical.Controllers
 {
@@ -11,78 +13,129 @@ namespace LaVentaMusical.Controllers
         // GET: Canciones
         public ActionResult Index()
         {
-            return View();
-        }
-
-        // GET: Canciones/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
+            using (var context = new LaVentaMusicalEntities())
+            {
+                var canciones = context.Canciones
+                                       .Include(c => c.Albumes)
+                                       .Include(c => c.Albumes.Artistas)
+                                       .Include(c => c.Generos)
+                                       .ToList();
+                return View(canciones);
+            }
         }
 
         // GET: Canciones/Create
         public ActionResult Create()
         {
+            PrepararSelectLists();
             return View();
         }
 
         // POST: Canciones/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Create(Canciones cancion)
         {
-            try
+            if (ModelState.IsValid)
             {
-                // TODO: Add insert logic here
+                try
+                {
+                    using (var context = new LaVentaMusicalEntities())
+                    {
+                        context.Canciones.Add(cancion);
+                        context.SaveChanges();
+                        return RedirectToAction("Index");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError("", "Error al agregar la canción: " + ex.Message);
+                }
+            }
 
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
+            PrepararSelectLists(cancion.Id_Genero, cancion.Id_Album);
+            return View(cancion);
         }
 
         // GET: Canciones/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            using (var context = new LaVentaMusicalEntities())
+            {
+                var cancion = context.Canciones.Find(id);
+                if (cancion == null)
+                {
+                    return HttpNotFound();
+                }
+
+                PrepararSelectLists(cancion.Id_Genero, cancion.Id_Album);
+                return View(cancion);
+            }
         }
 
         // POST: Canciones/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public ActionResult Edit(int id, Canciones cancionActualizada)
         {
-            try
+            if (ModelState.IsValid)
             {
-                // TODO: Add update logic here
+                try
+                {
+                    using (var context = new LaVentaMusicalEntities())
+                    {
+                        var cancion = context.Canciones.Find(id);
+                        if (cancion == null)
+                        {
+                            return HttpNotFound();
+                        }
 
-                return RedirectToAction("Index");
+                        cancion.Id_Genero = cancionActualizada.Id_Genero;
+                        cancion.Id_Album = cancionActualizada.Id_Album;
+                        cancion.Nombre_Cancion = cancionActualizada.Nombre_Cancion;
+                        cancion.Video_URL = cancionActualizada.Video_URL;
+                        cancion.Precio = cancionActualizada.Precio;
+                        cancion.Canciones_Disponibles = cancionActualizada.Canciones_Disponibles;
+
+                        context.SaveChanges();
+                        return RedirectToAction("Index");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError("", "Error al actualizar la canción: " + ex.Message);
+                }
             }
-            catch
+
+            PrepararSelectLists(cancionActualizada.Id_Genero, cancionActualizada.Id_Album);
+            return View(cancionActualizada);
+        }
+
+        // GET: Canciones/Details/5
+        public ActionResult Details(int id)
+        {
+            using (var context = new LaVentaMusicalEntities())
             {
-                return View();
+                var cancion = context.Canciones
+                                     .Include(c => c.Albumes)
+                                     .Include(c => c.Albumes.Artistas)
+                                     .Include(c => c.Generos)
+                                     .FirstOrDefault(c => c.Id_Cancion == id);
+
+                if (cancion == null)
+                {
+                    return HttpNotFound();
+                }
+
+                return View(cancion);
             }
         }
 
-        // GET: Canciones/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
 
-        // POST: Canciones/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
+        private void PrepararSelectLists(int? idGeneroSeleccionado = null, int? idAlbumSeleccionado = null)
         {
-            try
+            using (var context = new LaVentaMusicalEntities())
             {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
+                ViewBag.Id_Genero = new SelectList(context.Generos.ToList(), "Id_Genero", "Descripcion", idGeneroSeleccionado);
+                ViewBag.Id_Album = new SelectList(context.Albumes.ToList(), "Id_Album", "Nombre_Album", idAlbumSeleccionado);
             }
         }
     }
